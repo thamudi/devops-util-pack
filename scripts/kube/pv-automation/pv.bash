11fbfb708f15353977d205868a1509e07de5f59d
thamudi <tamim_hamoudi@outlook.com>
2025-02-04 13:12:09 +0300

    fix: correct shebang and improve formatting in pv.bash script

--- scripts/kube/pv-automation/pv.bash
+++ scripts/kube/pv-automation/pv.bash
@@ -1,9 +1,7 @@
-# /bin/bash
+#!/bin/bash
 
 PVs=$(kubectl get pv -o json | jq -r '.items[] | {pvName:.metadata.name, Capacity:.spec.capacity.storage, AccessMode:.spec.accessModes[0], NameSpace: .spec.claimRef.namespace, Name: .spec.claimRef.name, storageClass: .spec.storageClassName, creationTimeStamp: .metadata.creationTimestamp, claimPolicy: .spec.persistentVolumeReclaimPolicy}')
 # echo "$PVs" | sed 's/}\s*\{/}, {/g' > pv.json
-echo "$PVs" > pv.json
+echo "$PVs" >pv.json
 # sed -i -E 's/}\n\{/}, {/g' pv.json
 sed -i -E 's/}\s*\{/}, {/g' pv.json
-
-
